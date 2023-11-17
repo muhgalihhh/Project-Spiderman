a316ba693b6c36f7135a3f6cb51c4b0012b25e29
MUHAMAD GALIH <100691045+muhgalihhh@users.noreply.github.com>
2023-11-17 16:37:06 +0700

    Buat database
    Buat database nanti

--- db_spiderman.sql
+++ db_spiderman.sql
@@ -1,1 +1,29 @@
 CREATE DATABASE db_spiderman;
+
+CREATE TABLE
+    user (
+        id INT PRIMARY KEY,
+        username VARCHAR(50) NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        role VARCHAR(20) NOT NULL
+    );
+
+CREATE TABLE
+    film (
+        id_film INT PRIMARY KEY,
+        judul VARCHAR(100) NOT NULL,
+        MC VARCHAR(50),
+        deskripsi TEXT,
+        link_trailer VARCHAR(255),
+        link_film VARCHAR(255),
+        gambar VARCHAR(255)
+    );
+
+CREATE TABLE
+    favorite (
+        id_favorite INT PRIMARY KEY,
+        id_user INT,
+        id_film INT,
+        FOREIGN KEY (id_user) REFERENCES user (id),
+        FOREIGN KEY (id_film) REFERENCES film (id_film)
+    );

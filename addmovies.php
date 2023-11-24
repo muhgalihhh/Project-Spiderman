b12953b5a7f88a6d841b1e31b116f017291ab9b7
conansaa <khansa.delphi@gmail.com>
2023-11-24 21:51:53 +0700

    Update addmovies.php

--- addmovies.php
+++ addmovies.php
@@ -5,7 +5,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="addmovies.css">
         <title>Add Movies</title>
-        <!–[if lt IE 9]> <script src=”http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js”></script> <![endif]–>
     </head>
     <body>
         <div class="box">

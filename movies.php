d75598b8f0078e6c7686c0d922fe74113f8f194a
conansaa <khansa.delphi@gmail.com>
2023-11-24 21:55:08 +0700

    Update movies.php

--- movies.php
+++ movies.php
@@ -6,9 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="movies.css">
     <title>Movies</title>
-    <!–[if lt IE 9]>
-        <script src=”http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js”></script>
-        <![endif]–>
     </head>
     <body>
         <div class="header">

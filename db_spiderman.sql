5cbcdd40d3df81ac47e288fc951d2a60d81f05c8
MUHAMAD GALIH <100691045+muhgalihhh@users.noreply.github.com>
2023-12-01 18:18:06 +0700

    update db

--- db_spiderman.sql
+++ db_spiderman.sql
@@ -1,32 +1,151 @@
-CREATE DATABASE db_spiderman;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 01, 2023 at 12:17 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
-USE db_spiderman;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-CREATE TABLE
-    user (
-        id INT PRIMARY KEY AUTO_INCREMENT,
-        username VARCHAR(50) NOT NULL,
-        email VARCHAR(50) NOT NULL,
-        password VARCHAR(255) NOT NULL,
-        role VARCHAR(20) NOT NULL
-    );
 
-CREATE TABLE
-    film (
-        id_film INT PRIMARY KEY AUTO_INCREMENT,
-        judul VARCHAR(100) NOT NULL,
-        MC VARCHAR(50),
-        deskripsi TEXT,
-        link_trailer VARCHAR(255),
-        link_film VARCHAR(255),
-        gambar VARCHAR(255)
-    );
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-CREATE TABLE
-    favorite (
-        id_favorite INT PRIMARY KEY AUTO_INCREMENT,
-        id_user INT,
-        id_film INT,
-        FOREIGN KEY (id_user) REFERENCES user (id),
-        FOREIGN KEY (id_film) REFERENCES film (id_film)
-    );
+--
+-- Database: `db_spiderman`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `favorite`
+--
+
+CREATE TABLE `favorite` (
+  `id_favorite` int(11) NOT NULL,
+  `id_user` int(11) DEFAULT NULL,
+  `id_film` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `favorite`
+--
+
+INSERT INTO `favorite` (`id_favorite`, `id_user`, `id_film`) VALUES
+(10, 1, 2),
+(11, 3, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `film`
+--
+
+CREATE TABLE `film` (
+  `id_film` int(11) NOT NULL,
+  `judul` varchar(100) NOT NULL,
+  `MC` varchar(50) DEFAULT NULL,
+  `deskripsi` text DEFAULT NULL,
+  `rating` varchar(4) NOT NULL,
+  `link_trailer` varchar(255) DEFAULT NULL,
+  `link_film` varchar(255) DEFAULT NULL,
+  `gambar` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `film`
+--
+
+INSERT INTO `film` (`id_film`, `judul`, `MC`, `deskripsi`, `rating`, `link_trailer`, `link_film`, `gambar`) VALUES
+(2, 'Spider Man', 'Tobey Maguire', 'Peter Parker is a weak teenager and is often bullied by his friends.But Peter also has friends, namely Gwen Stacy and Harry Osborn.\\r\\nPeter himself lives with his aunt, Aunt May, since his uncle died.  Peter works as a freelance photographer at a newspaper publishing office called the Daily Bugle.', '8.9', 'https://youtu.be/t06RUxPbp_c?si=b4LzLjKVySdW1yC3', 'https://youtu.be/t06RUxPbp_c?si=b4LzLjKVySdW1yC3', 'images/poster-film/spiderman comics.jpeg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `email`, `password`, `role`) VALUES
+(1, 'admin1', 'admin.spiderman@gmail.com', '$2y$10$km1Tt47PzEugnvkZuuClvOM/uQ6FioZoGCAJdJ3aG7HRthlOlsU.S', 'admin'),
+(2, 'muhgalihhh', 'galihslank79@gmail.com', '$2y$10$NtO6JjdsfJpnVlmUDuC0a.q/gB5Po3.KOHub3Pc.0wycgOB0fjVdm', 'user'),
+(3, 'khansadelphi', 'khansa@gmail.com', '$2y$10$IA80DsWn5v/NaqebN7eD5uXxq8iC9OqtGZEwwIY4GI8o4v7v7AcMW', 'user');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `favorite`
+--
+ALTER TABLE `favorite`
+  ADD PRIMARY KEY (`id_favorite`),
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `favorite_ibfk_2` (`id_film`);
+
+--
+-- Indexes for table `film`
+--
+ALTER TABLE `film`
+  ADD PRIMARY KEY (`id_film`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `favorite`
+--
+ALTER TABLE `favorite`
+  MODIFY `id_favorite` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `film`
+--
+ALTER TABLE `film`
+  MODIFY `id_film` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `favorite`
+--
+ALTER TABLE `favorite`
+  ADD CONSTRAINT `favorite_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `favorite_ibfk_2` FOREIGN KEY (`id_film`) REFERENCES `film` (`id_film`) ON DELETE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
